669ad05b5455105402e67ec33bb94ec07a2070bb
The Full Stack Alchemist <alulayafet@gmail.com>
2024-01-31 06:40:11 +0000

    FINAL COMMIT

--- mTrap.py
+++ mTrap.py
@@ -66,18 +66,17 @@ plt.title('Magnetic Field Magnitude Along the z-axis for Different Radii')
 plt.legend()
 plt.grid(True)
 
-"""Part 2: Begin"""
+"""Part 2: Electron Motion in the Magnetic Trap"""
 from scipy.constants import e as e_charge, c, m_e, eV
 from scipy.constants import epsilon_0  # Vacuum permittivity
 
 # Define tau for the radiation reaction force factor
 tau = (e_charge**2) / (6 * np.pi * epsilon_0 * m_e * (c**3))
-# print(f'tau: {tau}')
-#Parameter Scanning setup. Setting up the grid for paramter scanning by defining the ranges for the emission angles and radial distances
+
+# Parameter Scanning setup. Setting up the grid for paramter scanning by defining the ranges for the emission angles and radial distances
 # Range of emission angles (in degrees) and radial distances (in meters)
 emission_angles = np.linspace(76, 89, num=10)  
 radial_distances = np.linspace(0, 0.024, num=7)  
-
 
 parameters = [(r, theta) for r in radial_distances for theta in emission_angles]
 
@@ -92,7 +91,6 @@ def lorentz_dirac(t, y):
         return np.zeros_like(y)
     
     gamma = 1 / np.sqrt(1 - v_squared / c**2)
-    # print(f'gamma: {gamma}')
     
     # Magnetic field calculation
     B_r1, B_z1 = magnetic_field(r, z, -0.1)  # Coil 1 at Z0 = -0.1 m
@@ -122,7 +120,6 @@ def lorentz_dirac(t, y):
          (mu[0]*mu[2]*gamma**4*tau**2 + mu[1]*gamma**2*tau) * (omega[2]*vy - omega[1]*vx)) / (gamma * M_magnitude)
     
     return [vx, vy, vz, ax, ay, az]
-
 
 # Compute constants outside of the function
 kinetic_energy_eV = 18.6e3
@@ -185,43 +182,26 @@ def worker_function(args):
     frequency = simulate_electron_motion(r, angle)
     return r, angle, frequency
 
-def collect_result(result):
-    global res
-    global progress_counter
-    res.append(result)
-    progress_counter.value += 1
-    print(f"{progress_counter.value} / {total_tasks}")
-
 if __name__ == "__main__":
 
     parameters = [(r, theta) for r in radial_distances for theta in emission_angles]
-    total_tasks = len(parameters)
 
+    with Pool() as pool:
+        results = pool.map(worker_function, parameters)
 
-    with Manager() as manager:
-        res = manager.list()  # List to store results
-        progress_counter = manager.Value('i', 0)  # Counter initialized to 0
-
-        with Pool() as pool:
-            # apply_async for the counter
-            for param in parameters:
-                pool.apply_async(worker_function, args=(param,), callback=collect_result)
-
-            pool.close()
-            pool.join()
-
-        # Convert the manager list back to a regular list
-        res = list(res)
-        
-    desired_angle = 76
-    desired_radial_distance = 0.024
-    
     # Convert results to arrays for plotting
-    r_values, angle_values, frequencies = zip(*res)
+    r_values, angle_values, frequencies = zip(*results)
 
+    # Finding the maximum and minimum frequencies greater than zero
+    max_frequency = max(frequencies)
+    min_frequency = min(f for f in frequencies if f > 0)
+
+    print(f"Maximum Frequency: {max_frequency} MHz")
+    print(f"Minimum Frequency (greater than zero): {min_frequency} MHz")
     # Convert radial distance to cm
+    
     r_values_cm = [r * 100 for r in r_values]
-
+    
     # Plotting 3D Plot
     from mpl_toolkits.mplot3d import Axes3D
     import matplotlib.pyplot as plt
@@ -232,4 +212,5 @@ if __name__ == "__main__":
     ax.set_xlabel('Radial Distance (cm)')
     ax.set_ylabel('Emission Angle (degrees)')
     ax.set_zlabel('Bounce Frequency (MHz)')
+
     plt.show()
